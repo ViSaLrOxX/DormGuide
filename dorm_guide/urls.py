10b94562dd833b1b5cb41ed9ad85d767577b5f53
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-03-27 10:42:06 +0000

    Update urls.py

--- dorm_guide/urls.py
+++ dorm_guide/urls.py
@@ -1,4 +1,4 @@
-"""dorm_guide_project URL Configuration
+"""dorm_guide URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from dorm_guide_app import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('dorm-guide/', include('dorm_guide_app.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
