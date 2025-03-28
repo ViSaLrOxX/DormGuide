e980a2c28c1a98cc1ffee0a6061d094ee6fc67bb
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-03-28 11:00:44 +0000

    Update views.py

--- dorm_guide_app/views.py
+++ dorm_guide_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from dorm_guidee_app.models import *
+from dorm_guide_app.models import *
 from django.contrib.auth import logout
 from dorm_guide_app.forms import ReviewForm
 
