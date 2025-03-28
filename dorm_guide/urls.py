55ba2fb54f3e8b107b52f879e00853d0256744fd
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-03-28 05:41:47 +0000

    Update urls.py

--- dorm_guide/urls.py
+++ dorm_guide/urls.py
@@ -22,7 +22,6 @@ from dorm_guide_app import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
     path('dorm-guide/', include('dorm_guide_app.urls')),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
