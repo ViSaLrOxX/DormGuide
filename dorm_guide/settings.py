3d2a0ba3fbd562c266be340687029df274089503
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-03-28 10:27:05 +0000

    Update settings.py

--- dorm_guide/settings.py
+++ dorm_guide/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'dorm_guide_app',
     'django_registration',
     'django_bootstrap5',
