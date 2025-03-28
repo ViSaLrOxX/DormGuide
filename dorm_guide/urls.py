0a71cce77bdaca933377c2ff609187e702a370c3
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-03-28 11:46:42 +0000

    Update urls.py

--- dorm_guide/urls.py
+++ dorm_guide/urls.py
@@ -7,7 +7,6 @@ from dorm_guide_app import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('registration.backends.simple.urls')),
     path('dorm-guide/', include('dorm_guide_app.urls')),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
