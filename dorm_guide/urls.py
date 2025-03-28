606e8e64fc8a5f496179b7e78b1aaf56d85c10fc
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-03-28 12:36:43 +0000

    Update urls.py

--- dorm_guide/urls.py
+++ dorm_guide/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from dorm_guide_app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('dorm-guide/', include('dorm_guide_app.urls')),
-    path('accounts/', include('allauth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('accounts/', include('registration.backends.default.urls')),  
+]
