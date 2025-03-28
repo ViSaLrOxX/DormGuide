6eb8bd7cc0e6631b55fdc273658dce3d262b1592
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-03-28 05:44:04 +0000

    Update models.py

--- dorm_guide_app/models.py
+++ dorm_guide_app/models.py
@@ -1,118 +1,158 @@
-from django.db import models
-from django.core.exceptions import ValidationError
-from django.utils.text import slugify
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
-from decimal import Decimal
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.utils import timezone
 
 class University(models.Model):
-    name = models.CharField(max_length=128)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, default=0)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=0)
-    description = models.TextField(default="Description")
-    picture = models.ImageField(upload_to="university_images/", blank=True, null=True)
-    website = models.URLField(blank=True, default="")
-    slug = models.SlugField(unique=True, blank=True)
-    
+    NAME_MAX_LENGTH = 128
+    DESCRIPTION_MAX_LENGTH = 2048
+    SYNOPSIS_MAX_LENGTH = 1024
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    latitude = models.DecimalField(
+        max_digits=8,
+        decimal_places=6,
+        validators=[
+            MaxValueValidator(90.0),
+            MinValueValidator(-90.0),
+        ],
+        default=0,
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[
+            MaxValueValidator(180.0),
+            MinValueValidator(-180.0),
+        ],
+        default=0,
+    )
+    description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH, default="Description")
+    picture = models.ImageField(upload_to='university_images', blank=True, null=True)
+    website = models.URLField(blank=True)
+    synopsis = models.CharField(max_length=SYNOPSIS_MAX_LENGTH, default="Synopsis")
+    slug = models.SlugField(unique=True)
+    email_domain = models.CharField(max_length=128, default="example.com")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(University, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Universities'
+
     def __str__(self):
         return self.name
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-
-    def clean(self):
-        if self.latitude > 90 or self.latitude < -90:
-            raise ValidationError("Latitude must be between -90 and 90")
-        if self.longitude > 180 or self.longitude < -180:
-            raise ValidationError("Longitude must be between -180 and 180")
-
 
 class Accommodation(models.Model):
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
-    name = models.CharField(max_length=128)
-    description = models.TextField()
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    rent_min = models.DecimalField(max_digits=9, decimal_places=2)
-    rent_max = models.DecimalField(max_digits=9, decimal_places=2)
-    avg_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
-    reviews_no = models.PositiveIntegerField(default=0)
-    slug = models.SlugField(unique=True, blank=True)
-    
-    def __str__(self):
-        return f"{self.university.name} - {self.name}"
+    NAME_MAX_LENGTH = 128
+    DESCRIPTION_MAX_LENGTH = 2048
+
+    university = models.ForeignKey(University, on_delete=models.CASCADE, blank=False)
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
+    description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH, default="Description")
+    latitude = models.DecimalField(
+        max_digits=8,
+        decimal_places=6,
+        validators=[
+            MaxValueValidator(90.0),
+            MinValueValidator(-90.0),
+        ],
+        default=0,
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[
+            MaxValueValidator(180.0),
+            MinValueValidator(-180.0),
+        ],
+        default=0,
+    )
+    rent_max = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.0),
+        ],
+        
+        null=True,
+        blank=True
+    )
+    rent_min = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.0),
+        ],
+        
+        null=True,
+        blank=True
+    )
+    avg_rating = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        validators=[
+            MaxValueValidator(5.0),
+            MinValueValidator(1.0),
+        ],
+        null=True,
+        blank=True
+    )
+    reviews_no = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+        ],
+        default=0
+    )
+    picture = models.ImageField(upload_to='accommodation_images', blank=True, null=True)
+    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
-        if self.rent_min > self.rent_max:
-            raise ValidationError("Rent Min cannot be greater than Rent Max.")
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
+        self.slug = slugify(self.name)
+        super(Accommodation, self).save(*args, **kwargs)
 
-    def clean(self):
-        if self.rent_min < 0 or self.rent_max < 0:
-            raise ValidationError("Rent values must be non-negative.")
-        if self.rent_min > self.rent_max:
-            raise ValidationError("Rent Min cannot be greater than Rent Max.")
-
-
-class Location(models.Model):
-    name = models.CharField(max_length=128)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    class Meta:
+        unique_together = ('university', 'name')
 
     def __str__(self):
-        return self.name
-
-
-class Property(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField()
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=9, decimal_places=2)
-    is_available = models.BooleanField(default=True)
-    photo = models.ImageField(upload_to='property_images/', blank=True, null=True)
-    price_per_month = models.DecimalField(max_digits=9, decimal_places=2)
-    address = models.CharField(max_length=255, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
-    rating = models.IntegerField()
-    comment = models.TextField()
-    picture = models.ImageField(upload_to='review_pictures/', null=True, blank=True)
-    is_anonymous = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return f'Review by {self.user} for {self.property}'
+        return self.university.__str__() + " - " + self.name
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
-    favourite_properties = models.ManyToManyField(
-        Property, blank=True
+    current_student = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Review(models.Model):
+    TITLE_MAX_LENGTH = 128
+    DESCRIPTION_MAX_LENGTH = 1024
+
+    accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, blank=False)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH)
+    isAnonymous = models.BooleanField(default=False)
+    picture = models.ImageField(upload_to='review_images', blank=True, null=True)
+    likes = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+        ],
+        default=0
     )
+    rating = models.IntegerField(
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1),
+        ],
+        default=5
+    )
+    datetime = models.DateTimeField(blank=False, default=timezone.now())
 
     def __str__(self):
-        return f'{self.user.username} Profile'
-
-
-class Favourites(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.accommodation.name}"
+        return self.accommodation.__str__() + " " + self.title + " by user " + self.user.__str__()
